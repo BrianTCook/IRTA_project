123f3abee1cb905cbda2f74ec03b41bbbe121f8f
Brian Cook <btcook@umich.edu>
2020-05-11 16:21:01 -0400

    has basic reading data function

--- main_irta.py
+++ main_irta.py
@@ -1,5 +1,22 @@
 from pyspark.ml import Pipeline
 import numpy as np
+import pandas as pd
+
+def read_data(user):
+    
+    if user == 'Brian':
+    
+        datadir = '/Users/BrianTCook/Desktop/quora-question-pairs/'
+    
+    train_df = pd.read_csv(datadir+'train.csv')
+    test_1_df = pd.read_csv(datadir+'test.csv')
+    test_2_df = pd.read_csv(datadir+'test_2.csv')
+    
+    return train_df, test_1_df, test_2_df
 
 if __name__ in '__main__':
-	print('hello, world!')
+    
+    user = 'Brian' #'Yuchi', 'Murad'
+    train, test_1, test_2 = read_data(user)
+    
+    print('hello, world!')
